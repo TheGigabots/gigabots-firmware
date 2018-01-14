00ad5e7b48d0e479583f9cc121fd5634a628edbb
tigeba <tigeba@altereality.com>
2018-01-14 00:05:15 -0600

    sleeps for updater.

--- tools/firmware_update.sh
+++ tools/firmware_update.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
 
+echo "##################"
+echo " FIRMWARE UPDATE  "
+echo "     STARTING     "
+echo "##################"
+sleep 5
 
 mkdir -p ./tmp
 curl -s https://api.github.com/repos/TheGigabots/gigabots-firmware/releases/latest \
@@ -7,5 +13,12 @@ curl -s https://api.github.com/repos/TheGigabots/gigabots-firmware/releases/late
   | xargs curl -L > ./tmp/src.tgz
 
 tar zxvf ./tmp/src.tgz --strip-components=1 -C ./tmp
-cp -r tmp/dist/ ../dist
+
+cp tmp/dist/* ../dist
 rm -rf ./tmp
+
+echo "##################"
+echo " FIRMWARE UPDATE  "
+echo "     COMPLETE     "
+echo "##################"
+sleep 5
